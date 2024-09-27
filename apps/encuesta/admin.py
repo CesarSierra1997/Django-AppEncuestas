174f031601d45e58d9d5309378baa013ddbf2b70
Cesar_Sierra <cesarmallungo@gmail.com>
2024-09-27 01:14:33 -0500

    Preparacion de archivos para implementación de los modelos, optimización y simplicidad del codigo

--- apps/encuesta/admin.py
+++ apps/encuesta/admin.py
@@ -1,23 +1,4 @@
 from django.contrib import admin
 from .models import *
 
-class PreguntaGeneralInline(admin.TabularInline):
-    model = PreguntaGeneral
-    extra = 0
-
-class PreguntaSelectMultipleInline(admin.TabularInline):
-    model = PreguntaSelectMultiple
-    extra = 0
-
-class PreguntaSiONoInline(admin.TabularInline):
-    model = PreguntaSiONo
-    extra = 0
-
-class PreguntaNumericaInline(admin.TabularInline):
-    model = PreguntaNumerica
-    extra = 0
-
-class EncuestaAdmin(admin.ModelAdmin):
-    inlines = [PreguntaGeneralInline, PreguntaSelectMultipleInline, PreguntaSiONoInline, PreguntaNumericaInline]
-
-admin.site.register(Encuesta, EncuestaAdmin)
+admin.site.register(Encuesta)
--- apps/encuesta/context_processors.py
+++
@@ -1,7 +0,0 @@
-from apps.encuesta.views import obtener_direccion_ip
-
-def direccion_ip(request):
-    # Obtener la dirección IP del usuario
-    ip_address = obtener_direccion_ip(request)
-    # Devuelve un diccionario con la dirección IP
-    return {'ip_address': ip_address}
