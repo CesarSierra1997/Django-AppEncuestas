a1f51cd46d4a971b589a1e68889678cb870319d9
Cesar_Sierra <cesarmallungo@gmail.com>
2024-09-29 22:29:05 -0500

    Ajuste general de settings.py - middleware

--- Encuesta/settings.py
+++ Encuesta/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'apps.encuesta.middleware.BlockSpecificRoutesMiddleware',
 ]
 
 ROOT_URLCONF = 'Encuesta.urls'
